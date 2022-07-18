0961afbdf36ebbfb658579cd58d1ae40523e0f02
Fernanda Ferreira <fefcufe@gmail.com>
2022-07-18 09:11:59 -0300

    feat: requisito 10

---
+++ src/database/models/blogPost.js
@@ -0,0 +1,49 @@
+module.exports = (sequelize, DataTypes) => {
+    const BlogPost = sequelize.define('BlogPost', {
+      id: { 
+        type: DataTypes.INTEGER, 
+        primaryKey: true, 
+        autoIncrement: true,
+        allowNull: false,
+        },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        },
+      content: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        },
+      userId: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          foreignKey: true,
+          references: {
+            model: 'Users',
+            key: 'id',
+          }
+        },
+      published: {
+        type: DataTypes.DATE,
+        allowNull: true,
+        },
+    
+      updated: {
+        type: DataTypes.DATE,
+        allowNull: true,
+        },
+    },
+    {
+      timestamps: false,
+      tableName: 'BlogPosts',
+    });
+  
+    BlogPost.associate = (models) => {
+      BlogPost.belongsTo(models.User,
+        { foreignKey: 'userId', as: 'user' });
+/*       BlogPost.hasMany(models.PostCategory,
+        { foreignKey: 'postId', as: 'postcategory' }); */
+    };
+    
+    return BlogPost;
+  };
